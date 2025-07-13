979915cd11991c890487aef7f02fccd6cf5beee9
JinuCheon <woojin8787@gmail.com>
2025-07-14 01:10:31 +0900

    audit LocalDateTime 으로 변경

--- src/main/kotlin/com/draw/it/api/common/entity/BaseEntity.kt
+++ src/main/kotlin/com/draw/it/api/common/entity/BaseEntity.kt
@@ -6,16 +6,16 @@ import jakarta.persistence.MappedSuperclass
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
-import java.time.OffsetDateTime
+import java.time.LocalDateTime
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntity (
     @CreatedDate
     @Column(name = "created_at", updatable = false)
-    var createdAt: OffsetDateTime? = null,
+    var createdAt: LocalDateTime? = null,
 
     @LastModifiedDate
     @Column(name = "updated_at")
-    var updatedAt: OffsetDateTime? = null
+    var updatedAt: LocalDateTime? = null
 )
