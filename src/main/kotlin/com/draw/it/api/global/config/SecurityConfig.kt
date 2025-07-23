fd97fd877f935c787346a5de3458686d058c2058
JinuCheon <woojin8787@gmail.com>
2025-07-23 23:20:35 +0900

    health check endpoint 는 security 통과

--- src/main/kotlin/com/draw/it/api/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/draw/it/api/global/config/SecurityConfig.kt
@@ -27,7 +27,7 @@ class SecurityConfig(
             .authorizeHttpRequests { auth ->
                 auth
                     .requestMatchers("/auth/**").permitAll()
-                    .requestMatchers("/api-docs/**", "/docs/**", "/swagger-ui/**").permitAll()
+                    .requestMatchers("/api-docs/**", "/docs/**", "/swagger-ui/**", "/health").permitAll()
                     .anyRequest().authenticated()
             }
             .addFilterBefore(authenticateJwt, UsernamePasswordAuthenticationFilter::class.java)
