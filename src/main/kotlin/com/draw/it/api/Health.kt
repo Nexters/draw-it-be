1da202783feb8a7b557df4a88af6041165eeee12
JinuCheon <woojin8787@gmail.com>
2025-07-23 23:25:41 +0900

    ResponseEntity 제거

--- src/main/kotlin/com/draw/it/api/Health.kt
+++ src/main/kotlin/com/draw/it/api/Health.kt
@@ -1,6 +1,5 @@
 package com.draw.it.api
 
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 class Health {
 
     @GetMapping("/health")
-    fun health(): ResponseEntity<String> {
-        return ResponseEntity.ok("OK")
+    fun health() {
+        return
     }
 }
